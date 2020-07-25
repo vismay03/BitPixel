ae8fdbf1076ae4fa2c7d5e73891485c98f07bb3e
vismay <vismaykatharani03@gmail.com>
2020-07-25 19:30:46 +0530

    eight

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-var bars = document.querySelector('.fa-bars');
-var nav = document.querySelector('.navbar');
-bars.addEventListener('click', function() {
-    if( nav.style.display == "none"){
-        nav.style.display = "block";
-    }
-    else {
-        nav.style.display = "none";
-    }
+// var bars = document.querySelector('.fa-bars');
+// var nav = document.querySelector('.navbar');
+// bars.addEventListener('click', function() {
+//     if( nav.style.display == "none"){
+//         nav.style.display = "block";
+//     }
+//     else {
+//         nav.style.display = "none";
+//     }
     
-}); 
+// }); 
